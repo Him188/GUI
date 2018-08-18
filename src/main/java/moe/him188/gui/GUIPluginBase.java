c007d22a3c608fc042314a588c3e5363a9199655
Him188 <Him188@mamoe.net>
2018-08-18 22:33:48 +0800

    test

--- src/main/java/moe/him188/gui/GUIPluginBase.java
+++ src/main/java/moe/him188/gui/GUIPluginBase.java
@@ -14,6 +14,7 @@ public class GUIPluginBase extends PluginBase {
     public GUIPluginBase() {
         super();
 
+
         instance = this;
         listenerManager = new ListenerManager();
     }
